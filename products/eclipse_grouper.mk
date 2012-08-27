7b5da655db670a0ad10870a9958b99019172e579
nitroglycerine33 <nitroglycerine33@gmail.com>
2012-08-27 12:42:43 -0500

    Update propietary files

--- products/eclipse_grouper.mk
+++ products/eclipse_grouper.mk
@@ -9,12 +9,19 @@ PRODUCT_BRAND := Google
 PRODUCT_MODEL := Nexus 7
 PRODUCT_MANUFACTURER := Asus
 
-
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.com.google.clientidbase=android-verizon \
     ro.com.google.locationfeatures=1 \
     ro.com.android.dataroaming=false \
     ro.setupwizard.mode=OPTIONAL \
+
+PRODUCT_COPY_FILES += \
+    vendor/asus/grouper/proprietary/media/LMprec_508.emd:system/media/LMprec_508.emd \
+    vendor/asus/grouper/proprietary/media/PFFprec_600.emd:system/media/PFFprec_600.emd \
+    vendor/asus/grouper/proprietary/media/video/AndroidInSpace.240p.mp4:system/media/video/AndroidInSpace.240p.mp4 \
+    vendor/asus/grouper/proprietary/media/video/AndroidInSpace.480p.mp4:system/media/video/AndroidInSpace.480p.mp4 \
+    vendor/asus/grouper/proprietary/media/video/Sunset.240p.mp4:system/media/video/Sunset.240p.mp4 \
+    vendor/asus/grouper/proprietary/media/video/Sunset.480p.mp4:system/media/video/Sunset.480p.mp4
 
 # Trinity Kernel - morfic
 PRODUCT_COPY_FILES +=  \
