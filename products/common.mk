a6bbdc84d15df79941e0ff650c2b2a5b1bc34930
nitroglycerine33 <nitroglycerine33@gmail.com>
2012-09-08 10:42:54 -0500

    Update to Build15 on prop

--- products/common.mk
+++ products/common.mk
@@ -88,6 +88,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.config.ringtone=Girtab.ogg \
     ro.config.notification_sound=Proxima.ogg \
     ro.config.alarm_alert=Cesium.ogg \
-    ro.modversion=Eclipse-Build12 \
+    ro.modversion=Eclipse-Build15 \
     ro.kernel.android.checkjni=0 \
     windowsmgr.max_events_per_sec=240
