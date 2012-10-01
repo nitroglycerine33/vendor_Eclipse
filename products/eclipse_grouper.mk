c23f1a9fc1f156638a82b7681fbdd7bfb85b7f17
nitroglycerine33 <nitroglycerine33@gmail.com>
2012-10-01 11:21:07 -0500

    Bring back grouper overlay for nav bar

--- products/eclipse_grouper.mk
+++ products/eclipse_grouper.mk
@@ -29,5 +29,5 @@ PRODUCT_COPY_FILES += \
 #PRODUCT_COPY_FILES +=  \
 #vendor/Eclipse/prebuilt/common/kernel/nexus7/boot.img:system/xbin/boot.img \
 
-#PRODUCT_PACKAGE_OVERLAYS += vendor/Eclipse/overlay/grouper
+PRODUCT_PACKAGE_OVERLAYS += vendor/Eclipse/overlay/grouper
 
