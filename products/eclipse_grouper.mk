6abe19f1d1499c832abe2dc80336a8633102f14d
nitroglycerine33 <nitroglycerine33@gmail.com>
2012-10-11 22:21:15 -0500

    Add back inline kernel

--- products/eclipse_grouper.mk
+++ products/eclipse_grouper.mk
@@ -26,8 +26,8 @@ PRODUCT_COPY_FILES += \
     vendor/asus/grouper/proprietary/media/video/Sunset.480p.mp4:system/media/video/Sunset.480p.mp4
 
 # Trinity Kernel
-PRODUCT_COPY_FILES +=  \
-vendor/Eclipse/prebuilt/common/kernel/nexus7/boot.img:system/xbin/boot.img \
+#PRODUCT_COPY_FILES +=  \
+#vendor/Eclipse/prebuilt/common/kernel/nexus7/boot.img:system/xbin/boot.img \
 
 PRODUCT_PACKAGE_OVERLAYS += vendor/Eclipse/overlay/grouper
 
