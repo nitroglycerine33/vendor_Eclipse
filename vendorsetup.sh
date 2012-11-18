f06ed06a4cf7543d8a20920665323eb7f685a7a1
nitroglycerine33 <nitroglycerine33@gmail.com>
2012-11-18 09:02:54 -0600

    Only building toro for now and removed a dead overlay

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,6 +1,1 @@
 add_lunch_combo eclipse_toro-userdebug
-add_lunch_combo eclipse_maguro-userdebug
-add_lunch_combo eclipse_grouper-userdebug
-add_lunch_combo eclipse_d2vzw-userdebug
-add_lunch_combo eclipse_toroplus-userdebug
-add_lunch_combo eclipse_fascinatemtd-userdebug
